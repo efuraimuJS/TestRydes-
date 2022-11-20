a0c5dcc8bb8d82e05d683bcda89b625e43d7f6e9
efuraimuJS <efuraimu.js@gmail.com>
2022-11-21 02:26:55 +0300

    adding custom js, css

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import "./custom/main"
 import "@hotwired/turbo-rails"
+
 import "controllers"
+
 
 import 'bootstrap'
 import 'jquery'
-import 'popperjs/core'
+import '@popperjs/core'
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,13 @@
 # Pin npm packages by running ./bin/importmap
-
+pin_all_from "app/javascript/custom", under: "custom", preload: true
 pin "application", preload: true
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.2/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.2/dist/js/bootstrap.esm.js", preload: true
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/controllers", under: "controllers", preload: true
+
+
+
